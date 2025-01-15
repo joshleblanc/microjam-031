ce31be2dcefc7f63f3e5d73db4031d61b08c16d4
Joshua LeBlanc <jleblanc@hey.com>
2025-01-14 20:56:17 -0400

    first phase boss position wasn't working

--- app/scripts/boss_script.rb
+++ app/scripts/boss_script.rb
@@ -12,6 +12,7 @@ module Scripts
     end
 
     def register_boss_position(position)
+      p "Registering boss position, #{position}, #{position.find_script_property(:phase)}"
       @positions[position.find_script_property(:phase)] = position
     end
 
@@ -24,6 +25,7 @@ module Scripts
     end
 
     def activate_phase(phase)
+      p "activating #{phase}, move to: #{@positions[phase]}"
       entity.move_to @positions[phase], 2000, Hoard::Tween::Type::ELASTIC_END
       entity.send("boss_attack_#{phase}_script".to_sym).activate!
     end

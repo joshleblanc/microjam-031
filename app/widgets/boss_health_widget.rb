6dc96ac95b68483b831695b1196e835cf7060e3c
Joshua LeBlanc <jleblanc@hey.com>
2025-01-14 20:53:23 -0400

    simplify

--- app/widgets/boss_health_widget.rb
+++ app/widgets/boss_health_widget.rb
@@ -1,32 +1,18 @@
 module Widgets
   class BossHealthWidget < Hoard::Widget
-    def initialize
-      super
-      show!
-    end
-
     def render
       health_script = entity.health_script
       return unless health_script
       health_percent = health_script.life.v.to_f / health_script.life.max
 
-      p "Rendering"
       window(
-        key: "health",
-        x: 100,
-        y: 20,
-        w: 1080,
-        h: 20,
-        padding: 0,
-        margin: 0,
+        x: 100, y: 20, w: 1080, h: 20,
+        padding: 0, margin: 0,
         background: { r: 60, g: 60, b: 60 },
         border: { r: 255, g: 255, b: 255 },
       ) do
         window(
-          w: 1080 * health_percent,
-          h: "100%",
-          padding: 0,
-          margin: 0,
+          w: 1080 * health_percent, h: "100%",
           background: { r: 200, g: 50, b: 50 },
         )
       end
